a3f53b7eeff0a501c254cc0dac1f1c5d873c991f
Dyllan Hope <dyllanjhope@gmail.com>
2019-05-07 16:37:30 +0200

    colour changing added

--- js/weekday_manager.js
+++ js/weekday_manager.js
@@ -1,31 +1,41 @@
 var firstDateElem = document.getElementById("first");
 var secondDateElem = document.getElementById("second");
 var weekdayListElem = document.getElementsByClassName(".weekday");
-var weekdays = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+var weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 var firstDay = '';
 var secondDay = '';
 
-window.onload = function(){
+window.onload = function () {
     whiten();
 }
-firstDateElem.onchange = function(){
+firstDateElem.onchange = function () {
     var firstDate = new Date(firstDateElem.value);
     firstDay = weekdays[firstDate.getDay()];
     var firstEle = document.getElementById(firstDay);
-    firstEle.style.backgroundColor = "blue";
+
+    if (firstDay === secondDay) {
+        firstEle.style.backgroundColor = "green";
+    } else {
+        firstEle.style.backgroundColor = "blue";
+    }
     whiten();
 }
-secondDateElem.onchange = function(){
+secondDateElem.onchange = function () {
     var secondDate = new Date(secondDateElem.value);
     secondDay = weekdays[secondDate.getDay()];
     var secondEle = document.getElementById(secondDay);
-    secondEle.style.backgroundColor = "red";
+
+    if (secondDay === firstDay) {
+        secondEle.style.backgroundColor = "green";
+    } else {
+        secondEle.style.backgroundColor = "red";
+    }
     whiten();
 }
-function whiten(){
-    for(var i=0;i<weekdays.length;i++){
+function whiten() {
+    for (var i = 0; i < weekdays.length; i++) {
         var ele = document.getElementById(weekdays[i]);
-        if((weekdays[i]!==firstDay)&&(weekdays[i]!==secondDay))
-        ele.style.backgroundColor = "white";
+        if ((weekdays[i] !== firstDay) && (weekdays[i] !== secondDay))
+            ele.style.backgroundColor = "white";
     }
 }
